50ea1cb965ebc461056931f40666775db1d8506d
Soumyajit Chakraborty <soumyo.chak1999@gmail.com>
2021-04-26 01:51:16 +0530

    v1.0.0.05 CUBIC

---
+++ Assets/scripts/groundspawnner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundspawnner : MonoBehaviour
+{
+    public GameObject groundplane;
+    Vector3 nextspwanpoint;
+
+    public void spwancube(){
+        GameObject temp = Instantiate(groundplane, nextspwanpoint, Quaternion.identity);
+
+        nextspwanpoint = temp.transform.GetChild(1).transform.position;
+    }
+
+    private void Start(){
+
+        for (long i = 1 ; i <= 10000 ; i++){
+            spwancube();
+        }
+     
+    }
+}
