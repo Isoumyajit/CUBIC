8772908a87d037d28b1a993ba72bab2301b04204
Soumyajit Chakraborty <soumyo.chak1999@gmail.com>
2021-04-26 15:37:57 +0530

    Obstacle Infinity Created

--- Assets/scripts/groundtile.cs
+++ Assets/scripts/groundtile.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 public class groundtile : MonoBehaviour
 {
         groundspawnner groundspwan;
+        public GameObject ObstaclePrefab;
 
         private void Start()
         {
             groundspwan = GameObject.FindObjectOfType<groundspawnner>();
+            spwanObstacle();
         }
 
         private void OnTriggerExit(Collider other) {
+            
             groundspwan.spwancube();
             Destroy(gameObject, 2); 
+        }
+
+        void spwanObstacle(){
+            
+            //choose random points to spwan obstacles
+             int getspwanIndex = Random.Range(2, 5);
+            Transform spwanIndex = transform.GetChild(getspwanIndex).transform;
+            //spwan the obstacle at that particular position
+
+            Instantiate(ObstaclePrefab, spwanIndex.position, Quaternion.identity, transform);
         }
 }
